c8985b5720d43f7ce4c75c055f772093575f9e94
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2022-12-29 15:36:59 +0700

    selection sort

--- Proyek/Siakad.java
+++ Proyek/Siakad.java
@@ -28,11 +28,31 @@ public class Siakad {
         Scanner input = new Scanner(System.in);
         System.out.println("Pilih Algoritma Pengurutan:");
         System.out.println("1. Exchange Sort");
+        System.out.println("2. Selection Sort");
+        System.out.println("3. Bubble Sort");
+        System.out.println("4. Quick Sort");
+        System.out.println("5. Shell Sort");
         System.out.print("Pilih algoritma: ");
         int pilihan = input.nextInt();
         switch (pilihan) {
             case 1: {
                 exchangeSort();
+                break;
+            }
+            case 2: {
+                selectionSort();
+                break;
+            }
+            case 3: {
+                bubbleSort();
+                break;
+            }
+            case 4: {
+                quickSort();
+                break;
+            }
+            case 5: {
+                shellSort();
                 break;
             }
         }
@@ -50,6 +70,69 @@ public class Siakad {
         }
     }
 
+    public static void selectionSort() {
+        for(int i = 0; i < jumlahData; i++){
+            int indeks = i;
+            for(int j = i + 1; j < jumlahData; j++){
+                if(mahasiswa[indeks].getNim().compareTo(mahasiswa[j].getNim()) >= 1){
+                    indeks = j;
+                }
+            }
+            if(indeks != i){
+                Mahasiswa temp = mahasiswa[i];
+                mahasiswa[i] = mahasiswa[indeks];
+                mahasiswa[indeks] = temp;
+            }
+        }
+    }
+
+    public static void bubbleSort() {
+        
+    }
+
+    public static void quickSort() {
+        
+    }
+
+    public static void shellSort() {
+        
+    }
+
+    public static void cariData() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Pilih Algoritma Pencarian:");
+        System.out.println("1. Linear Search");
+        System.out.println("2. Binary Search");
+        System.out.print("Pilih algoritma: ");
+        int pilihan = input.nextInt();
+        switch (pilihan) {
+            case 1: {
+                linearSearch();
+                break;
+            }
+            case 2: {
+                binarySearch();
+                break;
+            }
+        }
+    }
+
+    public static void linearSearch() {
+        
+    }
+
+    public static void binarySearch() {
+        
+    }
+
+    public static void editData() {
+        
+    }
+
+    public static void hapusData() {
+        
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int menu;
@@ -58,7 +141,10 @@ public class Siakad {
             System.out.println("1. Tambah data");
             System.out.println("2. Lihat Data");
             System.out.println("3. Urutkan Data");
-            System.out.println("4. Keluar");
+            System.out.println("4. Cari Data");
+            System.out.println("5. Edit Data");
+            System.out.println("6. Hapus Data");
+            System.out.println("7. Keluar");
             System.out.print("Pilih menu = ");
             menu = input.nextInt();
             if(menu == 1){
@@ -67,7 +153,13 @@ public class Siakad {
                 tampilData();
             } else if(menu == 3){
                 urutkanData();
+            } else if(menu == 4){
+                cariData();
+            } else if(menu == 5){
+                editData();
+            } else if(menu == 6){
+                hapusData();
             }
-        } while (menu != 4);
+        } while (menu != 7);
     }
 }
